b1ea79ef72baf2daad07743e951fc0606a087f69
jebinj97 <51470234+jebinj97@users.noreply.github.com>
2020-08-21 08:57:53 +0530

    Update views.py

--- polls/views.py
+++ polls/views.py
@@ -12,8 +12,9 @@ def index(request):
 
 
 def detail(request, question_id):
-    
+
     question = get_object_or_404(Question, pk=question_id)
+
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
@@ -22,6 +23,7 @@ def results(request, question_id):
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+    
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
